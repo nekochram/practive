fe697320214238d66292b4443dc15345b83ec571
helei_Eri <9933843+helei-eri@user.noreply.gitee.com>
2024-05-09 15:52:25 +0800

    完善大文件切片上传与合并切片

--- 大文件切片上传与后端合并切片/server/app.js
+++ 大文件切片上传与后端合并切片/server/app.js
@@ -1,4 +1,3 @@
-//后端这边还有bug，前端上传完切片请求后端/merge接口通知合并切片的时候会出问题，应该是读写的流程上有问题，后端还没接收完所有文件就开始合并，出现了问题
 const express = require("express");
 const multiparty = require("multiparty");
 const app = express();
@@ -32,21 +31,20 @@ const mergeChunks = (filename, sourceFile) => {
   const fileList = fs.readdirSync(sourceFile);
   const reg = /-(.*?)\./;
   fileList.sort((a, b) => {
-    return a.match(reg)[1] - b.match(reg)[1];
+    return a.match(reg)[1] - b.match(reg)[1]; //文件片段都是 测试大文件切片上传-0.mp4 这种格式，这里是匹配到 -后的序号，用序号来排序
   });
   const ws = fs.createWriteStream(`${sourceFile}/${filename}`);
   writeWithStreamLoop(fileList, ws, sourceFile);
 };
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.all("*", (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
   if (req.method === "OPTIONS") {
-    res.status = 200;
-    res.end();
+    res.status(200).send();
     return;
   }
   next();
@@ -57,8 +55,7 @@ app.post("/upload", (req, res) => {
     fs.mkdir(uploadDir, (err) => {
       if (err) {
         console.log(err);
-        res.statusCode = 500;
-        res.end("上传失败");
+        res.status(500).send({ code: 500, msg: "上传失败" });
         return;
       }
     });
@@ -69,35 +66,35 @@ app.post("/upload", (req, res) => {
   form.parse(req, (err, fields, files) => {
     if (err) {
       console.log(err);
-      res.statusCode = 500;
-      res.end("上传失败");
+      res.status(500).send({ code: 500, msg: "上传失败" });
       return;
     }
     const { index, hash, filename } = fields;
     const [name, suffix] = filename[0].split(".");
     const newPath = `${uploadDir}/${name}-${index}.${suffix}`;
-    const oldPath = `${uploadDir}/${files.file[0].path.split("\\")[1]}`;
+    const oldPath = `${uploadDir}/${files.file[0].path.split("\\")[1]}`; //文件统一命名成 测试大文件切片上传-0.mp4 这样
     //重命名为真实文件名
     fs.rename(oldPath, newPath, function (err) {
       if (err) {
         console.log("rename error: " + err);
-        res.status(500).end("{'status':500, 'message': '上传失败！'}");
+        res.status(500).send({ code: 500, msg: "上传失败" });
       } else {
-        res.status(200).end("{'status':200, 'message': '上传成功！'}");
+        res.status(200).send({ code: 200, msg: "上传成功" });
       }
     });
   });
 });
 
 //前端通知合成
-app.post("/merge", urlencodedParser,(req, res) => {
-  const { filename } = req.body;
+app.post("/merge", urlencodedParser, (req, res) => {
+  const { filename } = req.body; //
   try {
     mergeChunks(filename, uploadDir);
+    res.status(200).send({ code: 200, msg: "上传成功" });
   } catch (error) {
-    res.status(500).end("{'status':500, 'message': '上传失败！'}");
-  } finally {
-    res.status(200).end("{'status':200, 'message': '上传成功！'}");
+    console.log(error);
+    res.status(500).send({ code: 500, msg: "上传失败" });
   }
 });
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
