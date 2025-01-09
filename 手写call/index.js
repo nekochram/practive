3efe890149f1e1b32c2ee25f1ee0ae722c1b26ab
helei <9933843+helei-eri@user.noreply.gitee.com>
2025-01-09 14:20:36 +0800

    手写call

---
+++ 手写call/index.js
@@ -0,0 +1,17 @@
+Function.prototype.myCall = function (ctx, ...args) {
+  ctx = ctx === null || ctx === undefined ? globalThis : Object(ctx);
+  const key = Symbol();
+  Object.defineProperty(ctx, key, {
+    value: this,
+    enumerable: false,
+  });
+  const r = ctx[key](...args);
+  return r;
+};
+
+function method(a, b) {
+  console.log("args:", a, b);
+  console.log("this:", this);
+}
+
+method.myCall(1, 2, 3);
